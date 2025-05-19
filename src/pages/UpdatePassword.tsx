fe97e052deb5e5ba0e87f50f58ebc3df2a9630fe
Geoff Blaylock <gblaylock2024@gmail.com>
2025-05-18 21:14:11 -0700

    patch

--- src/pages/UpdatePassword.tsx
+++ src/pages/UpdatePassword.tsx
@@ -1,7 +1,7 @@
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { supabase } from "@/lib/supabase";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
